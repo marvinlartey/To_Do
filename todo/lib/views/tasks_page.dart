f4494451c86bc9de131b177738b0e184563d00dd
marvinlartey <larteymarv@gmail.com>
2021-11-30 10:33:42 +0000

    added list of tasks

--- todo/lib/views/tasks_page.dart
+++ todo/lib/views/tasks_page.dart
@@ -28,137 +28,196 @@ class TasksPage extends StatelessWidget {
         ],
       ),
       //BODY
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //WELCOME MESSAGE
-          const Padding(
-            padding: EdgeInsets.only(left: 33),
-            child: Text(
-              "What's up, Marvin!",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(34, 45, 87, 1)),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(33, 43, 0, 9),
-            child: Text('CATEGORIES',
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //WELCOME MESSAGE
+            const Padding(
+              padding: EdgeInsets.only(left: 33),
+              child: Text(
+                "What's up, Marvin!",
                 textAlign: TextAlign.left,
                 style: TextStyle(
-                    fontSize: 11,
+                    fontSize: 30,
                     fontWeight: FontWeight.bold,
-                    color: Color.fromRGBO(34, 45, 87, 1))),
-          ),
-          //TASKS CARDS
-          Row(
-            children: [
-              //CARD 1
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                height: MediaQuery.of(context).size.height * 0.15,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Card(
-                      color: Colors.white,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(10),
-                            child: Text('40 tasks',
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    color: Color.fromRGBO(164, 169, 199, 1))),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: Text('Business',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10.0),
-                            child: LinearPercentIndicator(
-                              width: 100.0,
-                              lineHeight: 3.0,
-                              percent: 0.5,
-                              progressColor: Colors.pink.shade300,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                    color: Color.fromRGBO(34, 45, 87, 1)),
               ),
-              // CARD 2
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                height: MediaQuery.of(context).size.height * 0.15,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Card(
-                      color: Colors.white,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(10),
-                            child: Text('18 tasks',
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    color: Color.fromRGBO(164, 169, 199, 1))),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: Text('Personal',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10.0),
-                            child: LinearPercentIndicator(
-                              width: 100.0,
-                              lineHeight: 3.0,
-                              percent: 0.7,
-                              progressColor: Colors.blue,
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(33, 43, 0, 9),
+              child: Text('CATEGORIES',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(34, 45, 87, 1))),
+            ),
+            //TASKS CARDS
+            Row(
+              children: [
+                //CARD 1
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Card(
+                        color: Colors.white,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text('40 tasks',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color.fromRGBO(164, 169, 199, 1))),
                             ),
-                          )
-                        ],
+                            const Padding(
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: Text('Business',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10.0),
+                              child: LinearPercentIndicator(
+                                width: 100.0,
+                                lineHeight: 3.0,
+                                percent: 0.5,
+                                progressColor: Colors.pink.shade300,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
+                // CARD 2
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Card(
+                        color: Colors.white,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text('18 tasks',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color.fromRGBO(164, 169, 199, 1))),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: Text('Personal',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10.0),
+                              child: LinearPercentIndicator(
+                                width: 100.0,
+                                lineHeight: 3.0,
+                                percent: 0.7,
+                                progressColor: Colors.blue,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
 
-          //SUBTITLE
-          const Padding(
-            padding: EdgeInsets.fromLTRB(33, 43, 0, 9),
-            child: Text('TODAY\'S TASKS ',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontSize: 11,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromRGBO(34, 45, 87, 1))),
-          ),
+            //SUBTITLE
+            const Padding(
+              padding: EdgeInsets.fromLTRB(33, 43, 0, 9),
+              child: Text('TODAY\'S TASKS ',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(34, 45, 87, 1))),
+            ),
 
-          //ListView
-        ],
+            //LIST OF TASKS
+            ConstrainedBox(
+              constraints:
+                  BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+              child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Card(
+                        child: Row(
+                      children: const [
+                        Radio(
+                            value: '',
+                            groupValue: '',
+                            onChanged: null,
+                            activeColor: Colors.pink),
+                        Text('Daily meeting with team')
+                      ],
+                    ));
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 5,
+                    );
+                  },
+                  itemCount: 22),
+            )
+
+            /* ListView.separated(
+                itemBuilder: ( context, index){
+                  return card(
+                    child: Row(
+                      children: [
+                        Radio(value: '', groupValue: '', onChanged: null, 
+                        activeColor: Colors.pink),
+                        Text('')
+                      ]
+                    )
+                  );
+                },
+      
+                separatorBuilder: (BuildContext context, int){},
+            ) */
+
+            /* Column(
+              children: const [
+                ListTile(
+                  leading: Radio(
+                    value: null,
+                    groupValue: null,
+                    onChanged: null,
+                    toggleable: true,
+                    activeColor: Colors.pink,
+                    focusColor: Colors.pink,
+                  ),
+                  title: Text("Daily meeting with team"),
+                  //trailing: Icon(Icons.favorite),
+                ),
+              ],
+            ) */
+          ],
+        ),
       ),
     );
   }
